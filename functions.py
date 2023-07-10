6a5c89c253c6147daf2470aa1979417790c72b2d
Augusto Mues <augustomues@gmail.com>
2023-07-10 21:49:10 +0200

    Big update. Streamilt and function script added. Folders organized

---
+++ functions.py
@@ -0,0 +1,135 @@
+import datetime
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import time
+
+
+
+
+def convert_time_to_decimal(time_string):
+    time_object = datetime.strptime(time_string, "%I:%M%p")
+    hour = time_object.hour
+    minute = time_object.minute
+    decimal_time = hour + minute / 60.0
+
+    return decimal_time
+
+
+
+
+def spliting_times(my_range):
+    period_start = my_range.split('–')[0]
+    period_finish = my_range.split('–')[1]
+    if period_start[-2:] == 'AM' or period_start[-2:] == 'PM':
+        pass
+    else:
+        period_start += period_finish[-2:]
+ 
+    period_start = convert_time_to_decimal(period_start)
+    period_finish = convert_time_to_decimal(period_finish)
+
+    if period_start > period_finish:
+        range1 = np.arange(period_start, 24, 0.25)
+        range2 = np.arange(0, period_finish, 0.25)
+        range = np.concatenate((range1, range2))
+    else:
+        range = np.arange(period_start, period_finish, 0.25)
+    return range
+
+
+
+
+def converting_times_to_ranges(row):
+    if row == None:
+        return np.nan
+
+    elif row == 'Closed':
+        return ['Closed']
+    
+    else:
+        try:
+            period_1 = row.split(",")[0].strip()
+            hours_opened = spliting_times(period_1)
+        except:
+            return ['Issue']
+       
+
+        try:
+            period_2 = row.split(",")[1].strip()
+            hours_opened_2 = spliting_times(period_2)
+            hours_opened = np.concatenate((hours_opened, hours_opened_2))
+        except IndexError:
+            pass
+        
+        try:
+            period_3 = row.split(",")[2].strip()
+            hours_opened_3 = spliting_times(period_3)
+            hours_opened = np.concatenate((hours_opened, hours_opened_3))
+        except IndexError:
+            pass
+
+        return hours_opened
+
+
+
+
+def restaurant_selector(df, raiting=None, max_price=None, total_reviews=None,
+                       neightbour=None, district=None, dine_in=['Yes'], reservable=None,
+                        serves_beer=None, serves_wine=None, vegetarian=None, takeout=None,
+                        wheelchair_accessible=None, day='Any', time=None, sorter=None, limit=10):
+
+    days = {'Monday': 'mon_hours'
+            ,'Tuesday': 'tue_hours'
+            ,'Wednesday': 'wed_hours'
+            ,'Thursday': 'tue_hours'
+            ,'Friday': 'fri_hours'
+            ,'Saturday': 'sat_hours'
+            ,'Sunday': 'sun_hours'}
+    
+    sorter_ord = {'total_reviews': False
+            , 'raiting': False
+            , 'price_level': True}
+
+    if raiting:
+        df = df[df['raiting'] >= raiting]
+    if max_price:
+        df = df[df['price_level'] <= max_price]
+    if total_reviews:
+        df = df[df['total_reviews'] >= total_reviews]
+    if neightbour:
+        df = df[df['neightbour'].isin(neightbour)]
+    if district:
+        df = df[df['distritos'].isin(district)]
+    if dine_in:
+        df = df[df['dine_in'].isin(dine_in)]
+    if reservable:
+        df = df[df['reservable'].isin(reservable)]
+    if serves_beer:
+        df = df[df['serves_beer'].isin(serves_beer)]
+    if serves_wine:
+        df = df[df['serves_wine'].isin(serves_wine)]
+    if vegetarian:
+        df = df[df['vegetarian'].isin(vegetarian)]
+    if takeout:
+        df = df[df['takeout'].isin(takeout)]
+    if wheelchair_accessible:
+        df = df[df['wheelchair_accessible'].isin(wheelchair_accessible)]
+    if day != 'Any':
+        hours_column = days[day]
+        df = df[df[hours_column].apply(lambda x: x != ['Closed'] if isinstance(x, list) else True)]
+    if time:
+        if day == 'Any':
+            raise KeyError('Invalid hour. If an hour is passed, a day must be passed as well')
+        else:
+            df = df[df.apply(lambda row: isinstance(row[days[day]], np.ndarray) and time in row[days[day]] if day and days.get(day) else False, axis=1)]
+    try:
+        sort_asc = []
+        for i in sorter:
+            sort_asc.append(sorter_ord[i])
+        df = df.sort_values(by=sorter, ascending=sort_asc)
+    except:
+        pass
+
+    return df[['name', 'direction', 'raiting', 'price_level', 'total_reviews', 'neightbour', 'distritos', 'latitud', 'longitud']].head(limit).reset_index(drop=True)
+    
---
+++ streamlit.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import functions as fnc
+
+st.set_page_config(page_title="Restaurant Selector", layout='centered', initial_sidebar_state='auto')
+
+st.markdown("<h1 style='text-align: center; color: black; font-size: 2em'>Welcome to Barcelona Restaurant Selector</h1>", unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns([2,6,2])
+with col1:
+    st.write("")
+with col2:
+    st.image("https://t1.uc.ltmcdn.com/es/posts/7/1/6/como_disfrutar_un_fin_de_semana_en_barcelona_24617_600.jpg")
+with col3:
+    st.write("")
+
+st.markdown("<h1 style='text-align: center; color: black; font-size: 22px'>Not sure where to go to eat or drink?</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black; font-size: large'>Let's get started!</h1>", unsafe_allow_html=True)
+
+df_1 = pd.read_csv('data/barc_restaurants.csv')
+df_2 = pd.read_csv('data/place_details.csv')
+df = df_1.merge(df_2, how='inner', on='place_id')
+
+distritos = list(df['distritos'].unique())
+barrios = list(df['neightbour'].unique())
+hours = np.arange(1, 25)
+
+for i in df.columns[8:]:
+    df[i] = df[i].apply(lambda x: fnc.converting_times_to_ranges(x))
+
+st.markdown("<h3> Please filter based on your needs.</h3><h6 style='font-size: 12px'>*Bare in mind that not all filters are needed. You can leave some fields empty.</h6>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center'>Let's start by the most important:</h3>", unsafe_allow_html=True)
+col1, col2, col3 = st.columns([6, 1, 6])
+vegeterian_opt =  []
+beer_option = []
+dine_in_option = []
+takeout_option = []
+wine_option = []
+wheelchair_available = []
+reservable = []
+days = []
+
+with col1:
+    price_max = st.slider("Which is the maximun price level you are looking for? Where 1=$ and 4=$$$$", 4, 1)
+    min_reviews = st.slider("How many reviews you want the restaurant to have at least?", 30000, 0, step=500)
+    avg_raiting = st.slider("What is the minimun avg. raiting you are looking for", 5.0, 0.0, step=0.1)
+
+with col3:
+    districts = st.multiselect('In which districts would you like to eat?', options=distritos)
+    neightbours = st.multiselect('To which Neightbour do you want to go?', options=barrios)
+    days = st.selectbox('Which day?', options=['Any', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+    hour = st.selectbox('At what time?', options=hours)
+
+st.markdown("<h3 style='text-align: center'>Do you have some extra requirments?</h3>", unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns([1, 1, 1])
+with col1:
+    vegeterian_option = st.multiselect('Vegeterian?', options=['Yes', 'No', 'Maybe'])
+    beer_option = st.multiselect('Serves beer?', options=['Yes', 'No', 'Maybe'])
+with col2:
+    dine_in_option = st.multiselect('Dine In?', options=['Yes', 'No', 'Maybe'])
+    takeout_option = st.multiselect('Takeout?', options=['Yes', 'No', 'Maybe'])
+    reservable = st.multiselect('Reservable?', options=['Yes', 'No', 'Maybe'])
+with col3:
+    wine_option = st.multiselect('Serves wine?', options=['Yes', 'No', 'Maybe'])
+    wheelchair_available = st.multiselect('Wheelchair accesible?', options=['Yes', 'No', 'Maybe'])
+
+st.dataframe(fnc.restaurant_selector(df, raiting=avg_raiting, max_price=price_max, total_reviews=min_reviews,
+                       neightbour=neightbours, district=districts, dine_in=dine_in_option, reservable=reservable,
+                        serves_beer=beer_option, serves_wine=wine_option, vegetarian=vegeterian_option, takeout=takeout_option,
+                        wheelchair_accessible=wheelchair_available, day=days, time=hour, sorter=None, limit=10))
+    
